9e6927e87ea0404721be2cafb85e9c89e65e70e2
Mitul Pravinbhai Malani <mt215690@dal.ca>
2022-04-06 17:55:56 -0300

    fix: updated base url for API

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  "https://gen2au88q1.execute-api.us-east-1.amazonaws.com/Production/";
+  "https://cxt9pq52gb.execute-api.us-east-1.amazonaws.com/Production/snapshare/";
